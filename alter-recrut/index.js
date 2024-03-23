576ebaa47949f2b156da9d6be7d595ce63513f8b
Tomizap <88057971+Tomizap@users.noreply.github.com>
2024-03-23 16:12:45 +0100

    Add files via upload

---
+++ alter-recrut/index.js
@@ -0,0 +1,29 @@
+const {api} = require('@tomizap/tools')
+const { google } = require("googleapis");
+
+const google_refresh_token = "1//039SrQdMLdNA9CgYIARAAGAMSNwF-L9IrHQztUaxZ04aM9ZlpKSR0oC3_GDgULTAqsrJrigaHesXx1hqIp3d4iYUm9yDp4j_F8Fw"
+
+async function main() {
+    const oauth2Client = new google.auth.OAuth2(
+        "1077742480191-sel8t74e6ivuu19m9r8h3aar15fd65r1.apps.googleusercontent.com", 
+        "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",
+        "http://localhost:3000/oauth/google/token"
+    );
+    await oauth2Client.setCredentials({
+        refresh_token: google_refresh_token, 
+    });
+    const googleapis = {
+        calendar: await google.calendar({ version: 'v3', auth: oauth2Client }),
+        drive: await google.drive({ version: 'v3', auth: oauth2Client }),
+        sheets: await google.sheets({ version: 'v4', auth: oauth2Client }),
+        drive: await google.drive({ version: 'v3', auth: oauth2Client }),
+        gmail: await google.gmail({ version: 'v1', auth: oauth2Client }),
+    }
+
+    await api.google.spreadsheet.rich(
+        "1ru8WO_rx1eV9x5S6I49RfJJdCpeVPkF8WAatVjhEDAw", 
+        'COMPANIES', 
+        googleapis.sheets)
+}
+main()
+
